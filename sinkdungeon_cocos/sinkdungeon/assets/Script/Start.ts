27fa11785df53d2fb326045bf4b3fd96f8a5adef
yulihzn <yuli.he@99bill.com>
2018-06-19 18:06:37 +0800

    change project to cocos

---
+++ sinkdungeon_cocos/sinkdungeon/assets/Script/Start.ts
@@ -0,0 +1,18 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    start () {
+        // init logic
+    }
+    startGame(){
+        cc.director.loadScene('game');
+    }
+}
