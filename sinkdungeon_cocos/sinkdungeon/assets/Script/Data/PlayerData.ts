06dcc4e2bbf50e1e583802e361f78f5ea301c1c0
yulihzn <330193303@qq.com>
2019-02-26 23:51:41 +0800

    fix

--- sinkdungeon_cocos/sinkdungeon/assets/Script/Data/PlayerData.ts
+++ sinkdungeon_cocos/sinkdungeon/assets/Script/Data/PlayerData.ts
@@ -15,9 +15,9 @@ import CommonData from "./CommonData";
 
 
 export default class PlayerData {
-    static DEFAULT_HEALTH = 5000;
+    static DEFAULT_HEALTH = 5;
     static DefAULT_SPEED = 300;
-    static DefAULT_ATTACK = 51;
+    static DefAULT_ATTACK = 1;
     name: string = '';
     pos: cc.Vec2 = cc.v2(7, 4);
 
