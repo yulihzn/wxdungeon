2ee261543e019f90af113225f0a961eaf9bde081
yulihzn <yuli.he@99bill.com>
2020-09-24 18:25:05 +0800

    try tiledmap

---
+++ sinkdungeon_resource/OtherTexure/map/world.tsx
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="16" tileheight="16" tilecount="68" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="277">
+  <image width="16" height="16" source="tile/dark.png"/>
+ </tile>
+ <tile id="278">
+  <image width="16" height="16" source="tile/water.png"/>
+ </tile>
+ <tile id="279">
+  <image width="16" height="16" source="tile/door0.png"/>
+ </tile>
+ <tile id="280">
+  <image width="16" height="16" source="tile/door1.png"/>
+ </tile>
+ <tile id="281">
+  <image width="16" height="16" source="tile/door2.png"/>
+ </tile>
+ <tile id="282">
+  <image width="16" height="16" source="tile/door3.png"/>
+ </tile>
+ <tile id="283">
+  <image width="16" height="16" source="tile/exitdoor0.png"/>
+ </tile>
+ <tile id="284">
+  <image width="16" height="16" source="tile/exitdoor1.png"/>
+ </tile>
+ <tile id="285">
+  <image width="16" height="16" source="tile/exitdoor2.png"/>
+ </tile>
+ <tile id="286">
+  <image width="16" height="16" source="tile/exitdoor3.png"/>
+ </tile>
+ <tile id="287">
+  <image width="16" height="16" source="tile/tile.png"/>
+ </tile>
+ <tile id="288">
+  <image width="16" height="16" source="tile/tilecorner0.png"/>
+ </tile>
+ <tile id="289">
+  <image width="16" height="16" source="tile/tilecorner1.png"/>
+ </tile>
+ <tile id="290">
+  <image width="16" height="16" source="tile/tilecorner2.png"/>
+ </tile>
+ <tile id="291">
+  <image width="16" height="16" source="tile/tilecorner3.png"/>
+ </tile>
+ <tile id="292">
+  <image width="16" height="16" source="tile/tilehalf0.png"/>
+ </tile>
+ <tile id="293">
+  <image width="16" height="16" source="tile/tilehalf1.png"/>
+ </tile>
+ <tile id="294">
+  <image width="16" height="16" source="tile/tilehalf2.png"/>
+ </tile>
+ <tile id="295">
+  <image width="16" height="16" source="tile/tilehalf3.png"/>
+ </tile>
+ <tile id="296">
+  <image width="16" height="16" source="tile/tileinner0.png"/>
+ </tile>
+ <tile id="297">
+  <image width="16" height="16" source="tile/tileinner1.png"/>
+ </tile>
+ <tile id="298">
+  <image width="16" height="16" source="tile/tileinner2.png"/>
+ </tile>
+ <tile id="299">
+  <image width="16" height="16" source="tile/tileinner3.png"/>
+ </tile>
+ <tile id="300">
+  <image width="16" height="16" source="tile/wall0.png"/>
+ </tile>
+ <tile id="301">
+  <image width="16" height="16" source="tile/wall1.png"/>
+ </tile>
+ <tile id="302">
+  <image width="16" height="16" source="tile/wall2.png"/>
+ </tile>
+ <tile id="303">
+  <image width="16" height="16" source="tile/wall3.png"/>
+ </tile>
+ <tile id="304">
+  <image width="16" height="16" source="tile/wallcorner0.png"/>
+ </tile>
+ <tile id="305">
+  <image width="16" height="16" source="tile/wallcorner1.png"/>
+ </tile>
+ <tile id="306">
+  <image width="16" height="16" source="tile/wallcorner2.png"/>
+ </tile>
+ <tile id="307">
+  <image width="16" height="16" source="tile/wallcorner3.png"/>
+ </tile>
+ <tile id="308">
+  <image width="16" height="16" source="tile/wallinner0.png"/>
+ </tile>
+ <tile id="309">
+  <image width="16" height="16" source="tile/wallinner1.png"/>
+ </tile>
+ <tile id="310">
+  <image width="16" height="16" source="tile/wallinner2.png"/>
+ </tile>
+ <tile id="311">
+  <image width="16" height="16" source="tile/wallinner3.png"/>
+ </tile>
+ <tile id="312">
+  <image width="16" height="16" source="tile/wallshort.png"/>
+ </tile>
+ <tile id="313">
+  <image width="16" height="16" source="tile/entrydoor0.png"/>
+ </tile>
+ <tile id="314">
+  <image width="16" height="16" source="tile/entrydoor1.png"/>
+ </tile>
+ <tile id="315">
+  <image width="16" height="16" source="tile/entrydoor2.png"/>
+ </tile>
+ <tile id="316">
+  <image width="16" height="16" source="tile/entrydoor3.png"/>
+ </tile>
+ <tile id="317">
+  <image width="16" height="16" source="tile/portal.png"/>
+ </tile>
+ <tile id="318">
+  <image width="16" height="16" source="tile/flag_a.png"/>
+ </tile>
+ <tile id="319">
+  <image width="16" height="16" source="tile/flag_b.png"/>
+ </tile>
+ <tile id="320">
+  <image width="16" height="16" source="tile/flag_c.png"/>
+ </tile>
+ <tile id="321">
+  <image width="16" height="16" source="tile/flag_d.png"/>
+ </tile>
+ <tile id="322">
+  <image width="16" height="16" source="tile/flag_e.png"/>
+ </tile>
+ <tile id="323">
+  <image width="16" height="16" source="tile/flag_f.png"/>
+ </tile>
+ <tile id="324">
+  <image width="16" height="16" source="tile/flag_g.png"/>
+ </tile>
+ <tile id="325">
+  <image width="16" height="16" source="tile/flag_h.png"/>
+ </tile>
+ <tile id="326">
+  <image width="16" height="16" source="tile/flag_i.png"/>
+ </tile>
+ <tile id="327">
+  <image width="16" height="16" source="tile/flag_j.png"/>
+ </tile>
+ <tile id="328">
+  <image width="16" height="16" source="tile/flag_k.png"/>
+ </tile>
+ <tile id="329">
+  <image width="16" height="16" source="tile/flag_l.png"/>
+ </tile>
+ <tile id="330">
+  <image width="16" height="16" source="tile/flag_m.png"/>
+ </tile>
+ <tile id="331">
+  <image width="16" height="16" source="tile/flag_n.png"/>
+ </tile>
+ <tile id="332">
+  <image width="16" height="16" source="tile/flag_o.png"/>
+ </tile>
+ <tile id="333">
+  <image width="16" height="16" source="tile/flag_p.png"/>
+ </tile>
+ <tile id="334">
+  <image width="16" height="16" source="tile/flag_q.png"/>
+ </tile>
+ <tile id="335">
+  <image width="16" height="16" source="tile/flag_r.png"/>
+ </tile>
+ <tile id="336">
+  <image width="16" height="16" source="tile/flag_s.png"/>
+ </tile>
+ <tile id="337">
+  <image width="16" height="16" source="tile/flag_t.png"/>
+ </tile>
+ <tile id="338">
+  <image width="16" height="16" source="tile/flag_u.png"/>
+ </tile>
+ <tile id="339">
+  <image width="16" height="16" source="tile/flag_v.png"/>
+ </tile>
+ <tile id="340">
+  <image width="16" height="16" source="tile/flag_w.png"/>
+ </tile>
+ <tile id="341">
+  <image width="16" height="16" source="tile/flag_x.png"/>
+ </tile>
+ <tile id="342">
+  <image width="16" height="16" source="tile/flag_y.png"/>
+ </tile>
+ <tile id="343">
+  <image width="16" height="16" source="tile/flag_z.png"/>
+ </tile>
+ <tile id="344">
+  <image width="16" height="16" source="tile/bed.png"/>
+ </tile>
+</tileset>
