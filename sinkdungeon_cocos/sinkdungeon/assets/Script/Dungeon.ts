7bfc0bc7ab470c7ac0f8db506f8e2ec081256bfa
yulihzn <yuli.he@99bill.com>
2020-10-12 18:08:43 +0800

    fix

--- sinkdungeon_cocos/sinkdungeon/assets/Script/Dungeon.ts
+++ sinkdungeon_cocos/sinkdungeon/assets/Script/Dungeon.ts
@@ -205,7 +205,7 @@ export default class Dungeon extends cc.Component {
                 //房间未清理时加载怪物
                 if (!Logic.mapManager.isCurrentRoomStateClear()
                     || Logic.mapManager.getCurrentRoomType().isEqual(RoomType.TEST_ROOM)) {
-                    // this.monsterManager.addMonstersAndBossFromMap(this, mapData[i][j], cc.v3(i, j));
+                    this.monsterManager.addMonstersAndBossFromMap(this, mapData[i][j], cc.v3(i, j));
                 }
 
             }
@@ -216,7 +216,7 @@ export default class Dungeon extends cc.Component {
         //加载随机怪物
         if (!Logic.mapManager.isCurrentRoomStateClear()
         && RoomType.isMonsterGenerateRoom(Logic.mapManager.getCurrentRoomType())) {
-            // this.monsterManager.addRandomMonsters(this);
+            this.monsterManager.addRandomMonsters(this);
         }
         this.setDoors(true,true);
         cc.log('load finished');
