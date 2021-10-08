d2a16ea011f0f3923406fb3ea5254b62287dab24
yulihzn <yuli.he@99bill.com>
2021-10-08 17:48:44 +0800

    fix

--- sinkdungeon_cocos/sinkdungeon/assets/Script/Test.ts
+++ sinkdungeon_cocos/sinkdungeon/assets/Script/Test.ts
@@ -13,19 +13,23 @@ export default class Test extends cc.Component {
     @property(cc.Graphics)
     graphics: cc.Graphics = null;
     @property(cc.Camera)
-    camera:cc.Camera = null;
+    camera: cc.Camera = null;
     @property(cc.Graphics)
-    ray:cc.Graphics = null;
+    ray: cc.Graphics = null;
     @property(cc.Sprite)
-    shadow:cc.Sprite = null;
+    shadow: cc.Sprite = null;
     @property(cc.Camera)
-    shadowCamera:cc.Camera = null;
+    shadowCamera: cc.Camera = null;
+    @property(cc.Node)
+    bg: cc.Node = null;
     private mat: cc.MaterialVariant;
     private mat1: cc.MaterialVariant;
+    private mat2: cc.MaterialVariant;
     radius = 200;
     playerPos: cc.Vec2 = cc.v2(640, 360);
-    private zoomArr = [1,0.5];
+    private zoomArr = [1, 0.5];
     private index = 0;
+    waveOffset: number = 0.0;
 
     // LIFE-CYCLE CALLBACKS:
 
@@ -39,8 +43,8 @@ export default class Test extends cc.Component {
             this.playerPos = pos.clone();
             cc.log(`playerPos:${pos.x},${pos.y}`);
         }, this)
-        
-        
+
+
         // this.ray.fillColor = cc.color(0,255,0,255);
         // this.ray.rect(-500,-500,1000,1000);
         // this.ray.fill();
@@ -49,16 +53,31 @@ export default class Test extends cc.Component {
         // this.ray.fill();
         // this.ray.circle(0,0,200);
         // this.ray.fill();
+
+        this.mat2 = this.bg.getComponent(cc.Sprite).getMaterial(0);
+        this.bg.on(cc.Node.EventType.TOUCH_END, (evt: cc.Event.EventTouch) => {
+            let pos = this.camera.getWorldToScreenPoint(evt.getLocation());
+            let canvasSize = cc.view.getCanvasSize();
+            let visibleSize = cc.view.getVisibleSize();
+            let visibleRatio = visibleSize.width / visibleSize.height;
+            let scale = canvasSize.width / visibleSize.width;
+            this.mat2.setProperty("canvas_size", cc.v2(canvasSize.width, canvasSize.height));
+            this.mat2.setProperty("wh_ratio", visibleRatio);
+            let center = cc.v2(pos.x / visibleSize.width, pos.y / visibleSize.height);
+            let y = Math.abs(center.y - 0.5) * visibleSize.height * scale / canvasSize.height;
+            this.mat2.setProperty("center", cc.v2(center.x, center.y > 0.5 ? 0.5 + y : 0.5 - y));
+            this.waveOffset = 0.0;
+        }, this);
     }
-    changeZoom(){
-        if(this.camera){
+    changeZoom() {
+        if (this.camera) {
             this.camera.zoomRatio = this.zoomArr[this.index++];
-            if(this.index>this.zoomArr.length-1){
+            if (this.index > this.zoomArr.length - 1) {
                 this.index = 0;
             }
         }
     }
-    changeRayCast(){
+    changeRayCast() {
     }
 
     start() {
@@ -66,8 +85,8 @@ export default class Test extends cc.Component {
         this.mat1 = this.ray.getMaterial(0);
         this.render(this.playerPos);
         let texture = new cc.RenderTexture();
-        texture.initWithSize(cc.visibleRect.width/8, cc.visibleRect.height/8);
-        texture.setFilters(cc.Texture2D.Filter.NEAREST,cc.Texture2D.Filter.NEAREST);
+        texture.initWithSize(cc.visibleRect.width / 8, cc.visibleRect.height / 8);
+        texture.setFilters(cc.Texture2D.Filter.NEAREST, cc.Texture2D.Filter.NEAREST);
         this.shadowCamera.targetTexture = texture;
         this.shadowCamera.render();
         this.shadow.spriteFrame = new cc.SpriteFrame(texture);
@@ -87,20 +106,20 @@ export default class Test extends cc.Component {
         // this.graphics.node.width = this.radius * 2;
         // this.graphics.node.height = this.radius * 2;
     }
-    private updateMat(mat: cc.MaterialVariant, pos: cc.Vec2,rayRadius:number) {
+    private updateMat(mat: cc.MaterialVariant, pos: cc.Vec2, rayRadius: number) {
         //画布宽高
         let canvasSize = cc.view.getCanvasSize();
         //视图宽高
         let visibleSize = cc.view.getVisibleSize();
         //视图比例
-        let visibleRatio = visibleSize.width /visibleSize.height;
+        let visibleRatio = visibleSize.width / visibleSize.height;
         //半径归一化
         let r = rayRadius / visibleSize.height;
         //实际缩放比例
         let scale = canvasSize.width / visibleSize.width;
         //传入画布宽高和视图比例计算对应的半径透明度，比例是为了画布防止宽高变形
         mat.setProperty("screen", cc.v2(canvasSize.width, canvasSize.height));
-        mat.setProperty("maxRadius", r*this.camera.zoomRatio);
+        mat.setProperty("maxRadius", r * this.camera.zoomRatio);
         mat.setProperty("whRatio", visibleRatio);
         //相机为原地的坐标归一化
         let lightPos = cc.v2(pos.x / visibleSize.width, pos.y / visibleSize.height);
@@ -114,8 +133,14 @@ export default class Test extends cc.Component {
     lateUpdate(dt) {
         this.render(this.playerPos);
         //传入的世界坐标减去相机的坐标得到对于相机为原点的坐标
-        let pos = this.camera.getWorldToScreenPoint(this.playerPos)
+        let pos = this.camera.getWorldToScreenPoint(this.playerPos);
         // this.updateMat(this.mat, cc.v2(this.playerPos.x - this.camera.node.x, this.playerPos.y - this.camera.node.y),this.radius);
-        this.updateMat(this.mat, cc.v2(pos.x,pos.y),this.radius);
+        this.updateMat(this.mat, cc.v2(pos.x, pos.y), this.radius);
+    }
+
+    update(dt) {
+        if (this.waveOffset > 2.0) return;
+        this.waveOffset += dt*2;
+        this.mat2.setProperty('wave_offset', this.waveOffset);
     }
 }
