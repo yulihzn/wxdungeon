54a198f5768e71236c67f67c7385911743f74902
yulihzn <yuli.he@99bill.com>
2021-10-12 18:03:28 +0800

    fix

---
+++ sinkdungeon_cocos/sinkdungeon/assets/Script/game/GameWorld.ts
@@ -0,0 +1,22 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class GameWorld extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
